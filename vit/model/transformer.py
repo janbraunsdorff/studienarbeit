11083e971419e01f725dbe120afec3c333936579
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-06 22:26:37 +0200

    change batch size

--- vit/model/transformer.py
+++ vit/model/transformer.py
@@ -15,7 +15,7 @@ class Transformer(nn.Module):
 
 
     def forward(self, encoded_patches):
-        print(encoded_patches.shape)
+        print(encoded_patches.shape, self.norm_1)
         x1 = self.norm_1(encoded_patches)
         attention_output = self.mha(x1,x1,x1)
         x2 = attention_output[0] + encoded_patches

803c01fb36b674701696e900746f8582251c836f
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-06 23:40:44 +0200

    change batch size

--- vit/trainer.py
+++ vit/trainer.py
@@ -57,7 +57,6 @@ class Trainer():
             batch_losses = [x['val_loss'] for x in eps]
             epoch_loss = np.average(batch_losses)
             batch_accs = [x['val_acc'] for x in eps]
-            print(sum)
             epoch_acc = (np.sum(batch_accs) / sum) * 100.0
 
             c4 = [x['c4'] for x in eps]
@@ -72,7 +71,7 @@ class Trainer():
         batch_losses = [x['val_loss'] for x in eps]
         epoch_loss = np.average(batch_losses)
         batch_accs = [x['val_acc'] for x in eps]
-        epoch_acc = np.sum(batch_accs) / 126.11
+        epoch_acc = np.average(batch_accs) 
 
         c4 = [x['c4'] for x in eps]
         c4 = np.sum(c4)
@@ -116,7 +115,7 @@ class Trainer():
         batch_losses = [x['val_loss'] for x in eps]
         epoch_loss = np.average(batch_losses)
         batch_accs = [x['val_acc'] for x in eps]
-        epoch_acc = (np.sum(batch_accs) / 12611) * 100.0
+        epoch_acc = np.average(batch_accs)
 
         c4 = [x['c4'] for x in eps]
         c4 = np.sum(c4)

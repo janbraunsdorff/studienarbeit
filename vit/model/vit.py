6c05c4d79fa2c5313100687f1431789b4ad91e8f
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-06 22:07:30 +0200

    change batch size

--- vit/model/vit.py
+++ vit/model/vit.py
@@ -49,7 +49,7 @@ class ViT(nn.Module):
         encode_patches = self.encode_patches(patches)
         # encode_patches = B x 144 x 64
 
-        print('pre_tansformers', encode_patches.shpae)
+        print('pre_tansformers', encode_patches.shape)
 
         for t in self.transformers:
             encode_patches = t(encode_patches)

e672800a263db985b6ce163c1a4265dc28a1a0a1
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-06 21:42:03 +0200

    change batch size

--- vit/model/data_augmentation.py
+++ vit/model/data_augmentation.py
@@ -3,9 +3,9 @@ import vit.model.config as conf
 
 data_augmentation = transforms.Compose(
     [
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Normalize((0.5), (0.5)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(degrees=0.2),
-        transforms.RandomResizedCrop(size=(conf.image_size, conf.image_size), )
+        transforms.RandomResizedCrop(size=(conf.image_size, conf.image_size))
     ]
 )

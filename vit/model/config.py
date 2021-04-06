303284d9608cae39cfab577ef8cc4b65418c1b09
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-07 01:05:31 +0200

    change batch size

--- vit/model/config.py
+++ vit/model/config.py
@@ -8,14 +8,15 @@ image_size = 256
 patch_size = 16
 num_patches = (image_size // patch_size) ** 2
 project_dim = 128
-num_heads = 4
+num_heads = 8
 hidden_layers = [
     (project_dim, project_dim*2), 
+    (project_dim*2, project_dim*2), 
     (project_dim*2, project_dim*2), 
     (project_dim*2, project_dim)
 ]
 transformer_layers = 16
-mlp_head_units = [4096, 2048, 1024]
+mlp_head_units = [4096, 2048, 2048, 1024]
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print('Device: ', device)

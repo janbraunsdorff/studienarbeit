5219efb6700be5c14503e0874a33a6946a794109
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-08 19:06:42 +0200

    change batch size

--- vit/model/config.py
+++ vit/model/config.py
@@ -15,7 +15,7 @@ hidden_layers = [
     (project_dim*2, project_dim*2), 
     (project_dim*2, project_dim)
 ]
-transformer_layers = 16
+transformer_layers = 8
 # mlp_head_units = [4096, 2048, 2048, 1024]
 res_file = 'result-first.csv'
 

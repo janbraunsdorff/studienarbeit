879b578eec7d8c8737688eb12551b938a2627172
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-18 19:26:19 +0200

    change batch size

--- vit/model/config.py
+++ vit/model/config.py
@@ -2,7 +2,7 @@ import torch
 
 lr = 1e-4
 weight_decay = 1e-5
-batch_szie = 16
+batch_szie = 64
 num_epoch = 10_000
 image_size = 256
 patch_size = 16
@@ -15,7 +15,7 @@ hidden_layers = [
     #(project_dim*2, project_dim*2), 
     (project_dim*2, project_dim)
 ]
-transformer_layers = 6
+transformer_layers = 3
 res_file = 'result-trans-8-small-hidden.csv'
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")

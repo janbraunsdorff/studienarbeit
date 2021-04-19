01f48e7acc70004d828b7fb078dd894ba7d7c896
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-19 14:00:58 +0200

    change batch size

--- vit/model/config.py
+++ vit/model/config.py
@@ -5,10 +5,10 @@ weight_decay = 1e-5
 batch_szie = 32
 num_epoch = 10_000
 image_size = 256
-patch_size = 8
+patch_size = 16
 num_patches = (image_size // patch_size) ** 2
-project_dim = 160
-num_heads = 16 
+project_dim = 64
+num_heads = 4
 hidden_layers = [
     (project_dim, project_dim*2), 
     (project_dim*2, project_dim)
--- vit/model/transformer.py
+++ vit/model/transformer.py
@@ -7,8 +7,8 @@ from vit.model.multilayerPerceptron import MultilayerPerceptron
 class Transformer(nn.Module):
     def __init__(self, patch_size):
         super(Transformer, self).__init__()
-        self.norm_1 = nn.BatchNorm1d(num_features=1024, eps=1e-6)
-        self.norm_2 = nn.BatchNorm1d(num_features=1024, eps=1e-1)
+        self.norm_1 = nn.BatchNorm1d(num_features=256, eps=1e-6)
+        self.norm_2 = nn.BatchNorm1d(num_features=267, eps=1e-1)
         self.mha = nn.MultiheadAttention(embed_dim=conf.project_dim, num_heads=conf.num_heads, dropout=0.1).to(conf.device)
         self.mlp = MultilayerPerceptron(dropout_rate=0.1, layers=conf.hidden_layers)
         self.to(conf.device)

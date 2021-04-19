d95baf3e37976f1196ab0b0804b04576e3d2f2d2
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-19 19:18:21 +0200

    change batch size

--- vit/model/transformer.py
+++ vit/model/transformer.py
@@ -24,7 +24,6 @@ class Transformer(nn.Module):
         x3 = self.mlp(x3)
         ret = x3 + x2
 
-        print(x1.shape)
         print(attention_output[1].shape)
         print(attention_output[1])
 

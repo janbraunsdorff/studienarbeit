3538517cb4cd4b38deb3d5ba509f7d4f64847501
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-19 13:54:59 +0200

    change batch size

--- vit/model/config.py
+++ vit/model/config.py
@@ -7,8 +7,8 @@ num_epoch = 10_000
 image_size = 256
 patch_size = 8
 num_patches = (image_size // patch_size) ** 2
-project_dim = 128
-num_heads = 32 
+project_dim = 160
+num_heads = 16 
 hidden_layers = [
     (project_dim, project_dim*2), 
     (project_dim*2, project_dim)

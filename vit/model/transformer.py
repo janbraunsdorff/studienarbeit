93b8eca76eef3fa9d27f3dd868c81522f8cbe492
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-19 14:01:22 +0200

    change batch size

--- vit/model/transformer.py
+++ vit/model/transformer.py
@@ -8,7 +8,7 @@ class Transformer(nn.Module):
     def __init__(self, patch_size):
         super(Transformer, self).__init__()
         self.norm_1 = nn.BatchNorm1d(num_features=256, eps=1e-6)
-        self.norm_2 = nn.BatchNorm1d(num_features=267, eps=1e-1)
+        self.norm_2 = nn.BatchNorm1d(num_features=256, eps=1e-1)
         self.mha = nn.MultiheadAttention(embed_dim=conf.project_dim, num_heads=conf.num_heads, dropout=0.1).to(conf.device)
         self.mlp = MultilayerPerceptron(dropout_rate=0.1, layers=conf.hidden_layers)
         self.to(conf.device)
@@ -16,7 +16,6 @@ class Transformer(nn.Module):
 
 
     def forward(self, encoded_patches):
-        print(encoded_patches.shape)
         x1 = self.norm_1(encoded_patches)
         attention_output = self.mha(x1,x1,x1)
         x2 = attention_output[0] + encoded_patches

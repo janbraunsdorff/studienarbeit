24bbf75b367c3c989b8cc8389948590803bbbc89
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-19 17:40:19 +0200

    change batch size

--- vit/model/vit.py
+++ vit/model/vit.py
@@ -55,7 +55,8 @@ class ViT(nn.Module):
             encoding, att = t(encoding)
         # encode_patches = B x 144 x 64 
 
-        print(att.squeeze(1).reshape(-1, 16, 16).shape)
+        print(att.squeeze(1).shape)
+        print(att.squeeze(1).reshape(64, 16, 16).shape)
         print(torch.argmax(att[0]))
         raise Exception('nö')
 

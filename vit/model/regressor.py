b444b274f991d86c48bad606ce725d74775ba758
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-20 11:48:14 +0200

    change batch size

--- vit/model/regressor.py
+++ vit/model/regressor.py
@@ -8,12 +8,13 @@ class Regreesor(nn.Module):
         super(Regreesor, self).__init__()
         self.activate = nn.LeakyReLU()
         self.norm = nn.BatchNorm2d(num_features=1)
-        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3,3))
-        self.conv_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3))
-        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3))
+        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(2,2))
+        self.conv_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(2,2))
+        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(2,2))
         self.max_Pool = nn.MaxPool2d(kernel_size=(2,2))
 
         self.inceptionA_1 = InceptionA(128, 256)
+        self.inceptionA_2 = InceptionA(128, 256)
 
     def forward(self, x):
         x = self.norm(x)
@@ -25,8 +26,9 @@ class Regreesor(nn.Module):
         x = self.activate(x)
         x = self.max_Pool(x)
         
-        print(x.shape)
         x = self.inceptionA_1(x)
+        print(x.shape)
+        x = self.inceptionA_2(x)
         print(x.shape)
 
         raise Exception('Regressor end')

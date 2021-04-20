e3a589471e293cf018584b850da0297f38842f43
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-20 11:13:17 +0200

    change batch size

--- vit/model/vit.py
+++ vit/model/vit.py
@@ -62,7 +62,8 @@ class ViT(nn.Module):
         max_value = torch.max(t, dim=1)[0]
         t = t / max_value.view(-1,1)
         t = t.view(-1, 16, 16)
-        print(t.shape, '\n', t)
+        mask = self.scale_maks(t)
+        print(t.shape, mask.shape)
 
 
         return x

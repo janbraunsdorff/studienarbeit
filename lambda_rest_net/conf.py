9322646ffecc2e66a97238a6ec64eaa02ee9c5d8
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-23 14:47:54 +0200

    change batch size

--- lambda_rest_net/conf.py
+++ lambda_rest_net/conf.py
@@ -4,7 +4,7 @@ batch_size = 16
 lr = 1e-4
 num_epoch = 1_000
 
-res_file = 'lambda-resnet-small.csv'
+res_file = 'lambda-resnet-small-big-image.csv'
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print('Device: ', device)

8e8ff50584acdc8177f3e1301434be9a9c2f2207
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-29 14:14:35 +0200

    change batch size

--- lambda_rest_net/model/net.py
+++ lambda_rest_net/model/net.py
@@ -20,7 +20,7 @@ class Net(nn.Module):
 
         # Regressor
         self.reg_1 = self.build_regessor(in_features=age_nurones+res_net_out, out_features=1500)
-        self.reg_2 = self.build_regessor(in_features=1000, out_features=1000)
+        self.reg_2 = self.build_regessor(in_features=1500, out_features=1000)
         self.reg_3 = self.build_regessor(in_features=1000, out_features=1000)
 
 

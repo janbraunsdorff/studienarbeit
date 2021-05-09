8c9c0ef0e60ef7e9756f8e3f1dd8d0e9b17f8a41
janbraunsdorff <jan99braunsdorff@web.de>
2021-05-09 19:14:11 +0200

    change batch size

--- v3/net.py
+++ v3/net.py
@@ -27,7 +27,8 @@ class MnistModel(nn.Module):
         self.inception_v3.fc = Identity()                                                               # remove classification layer
         self.inception_v3.Conv2d_1a_3x3.conv = nn.Conv2d(1, 32, 3,2)                                    # remove color channels
         self.inception_v3._transform_input = _transform_input                                           # change normalisation
-        # self.Mixed_5d = Identity()
+        self.Mixed_5d = Identity()
+        self.Mixed_6a = Identity()
 
         self.dense32 = nn.Linear(1, 64)
 

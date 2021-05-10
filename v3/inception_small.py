537a08760a83466be4e3eaadb20be164979ea384
janbraunsdorff <jan99braunsdorff@web.de>
2021-05-10 10:50:58 +0200

    change batch size

--- v3/inception_small.py
+++ v3/inception_small.py
@@ -285,6 +285,7 @@ class BasicConv2d(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, padding:int = 0, weights: torch.nn.Parameter = None):
         super(BasicConv2d, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, bias=False, kernel_size=kernel_size, stride=stride, padding=padding)
+        print(weights)
         if weights:
             self.conv.weight = weights
         self.bn = nn.BatchNorm2d(out_channels, eps=0.001)

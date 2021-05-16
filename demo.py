ad7f97d463d2eeaea47fe33a24c0470c11900a34
janbraunsdorff <jan99braunsdorff@web.de>
2021-05-16 13:04:10 +0200

    change batch size

--- demo.py
+++ demo.py
@@ -1,12 +1,19 @@
 import torch
 import torchvision
 
+from v3.processing import img_to_tensor
 
-def run(model_path, image_path):
+
+def run(model_path, image_path, sex):
     model = torch.load(model_path, map_location=torch.device('cpu'))
     model.eval()
 
+    img  = img_to_tensor(image_path).squeeze(0)
+    print(model(img, 0))
+
+
+
 
 if __name__ == "__main__":
-    run('./model/v3-small-row-11.pth', '')
+    run('./model/v3-small-row-11.pth', '../data/boneage-test-dataset/4360.png', 0)
     print('done')
